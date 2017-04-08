8bb6bf870ba7a869ca3e26d65e3b901c5e58c3f1
Roznam <catchpole_1@hotmail.com>
2017-04-08 21:12:15 +0100

    Nested menu functionality working
    Nested menus are now functioning as expected, files are still not
    pulling through

--- MultiClip - SYSTRAY/multiclip systray.py
+++ MultiClip - SYSTRAY/multiclip systray.py
@@ -1,77 +1,51 @@
-'''
-Intended to run within the system tray -
-
-- Looks at a given folder and creats a nested menu context menu when clicking.
-- Nested menu will show all folders/files within the parent directory.
-- Selecting a folder will open a new nested menu for that folder.
-- Selecting a file will copy the file contents to the clipboard.
-'''
-
-import sys
-import os
+import sys, os
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 mc_dir = "D:\\Python\\MC\\"
 
-
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
-
-    menu_items = []
 
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
         menu = QtWidgets.QMenu(parent)
-        exitAction = menu.addAction("Exit",)
+        exitAction = menu.addAction("Exit")
         exitAction.triggered.connect(parent.close)
 
-    def list_subfolders_gui(mc_dir):
-        for object in os.listdir(mc_dir):
-            object_path = mc_dir + object
-            if os.path.isdir(object_path):
-                dirEntry = menu.addMenu(object)
-
-    def list_files_gui(mc_dir):
-        for object in os.listdir(mc_dir):
-            object_path = mc_dir + object
-            if os.path.isfile(object_path):
-                fileAction = menu.addAction(object)
-                fileAction.triggered.connect(self.testing_func)
+        self.list_subfolders(mc_dir, menu)
 
         self.setContextMenu(menu)
 
     subfolders = []
-    def list_subfolders(path):
+
+    def list_subfolders(self, path, menu):
         for i in os.listdir(path):
             object_path = path +'/'+ i
             if os.path.isdir(object_path) is True:
-                print ">> Folder : ", object_path
-                # subfolders.append(object_path)
-                self.list_files(object_path)
-                print '\n'
-                self.list_subfolders(object_path)
+                print (">> Folder : ", object_path)
+                subfolder_menu = self.create_submenu(i, menu)
+                #subfolders.append(object_path)
+                #self.list_files(object_path)
+                print ('\n')
+                self.list_subfolders(object_path, subfolder_menu)
 
-    def list_files(path):
+    def list_files(self, path):
          for i in os.listdir(path):
              object_path = path +'/'+ i
              if os.path.isfile(object_path) is True:
-                 print object_path
+                 print (object_path)
 
-
-    def testing_func(self):
-        snd = self.sender().text()
-        print(f"detected: {snd}")
-
+    def create_submenu(self, i, menu):
+        subfolder_menu = menu.addMenu(i)
+        return subfolder_menu
 
 def main(image):
     app = QtWidgets.QApplication(sys.argv)
-
     w = QtWidgets.QWidget()
     trayIcon = SystemTrayIcon(QtGui.QIcon(image), w)
-    trayIcon.list_subfolders(mc_dir)
 
     trayIcon.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    on = r'D:\Python\mc.ico'
+    on=r'D:\Python\mc.ico'
     main(on)
