1dd7a10a6fcd2ca9bc36bb8270b0c52a02449b1a
Roznam <catchpole_1@hotmail.com>
2017-04-09 02:14:58 +0100

    Implemented copy to clipboard - issue with filenames / sender
    On initial setup of copy_contents - program crashed on opening files.
    Unable to open the specified file. Changed argument used within
    list_files. Specified details within comments.

--- MultiClip - SYSTRAY/multiclip systray.py
+++ MultiClip - SYSTRAY/multiclip systray.py
@@ -1,17 +1,16 @@
 '''
-Intended to run within the system tray -
-
-- Looks at a given folder and creats a nested menu context menu when clicking.
+Runs within the system tray -
+- Looks at a given folder (mc_dir) and creates a nested contexton right-click.
 - Nested menu will show all folders/files within the parent directory.
-- Selecting a folder will open a new nested menu for that folder.
-- Selecting a file will copy the file contents to the clipboard.
+- Selecting a folder opens a new nested menu for that folder.
+- Selecting a file copies the file contents to the clipboard.
 '''
 
-import sys
-import os
+import sys, os, pyperclip
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 mc_dir = "D:\\Python\\MC\\"
+systray_icon = "D:\\Python\\mc.ico"
 
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
@@ -20,9 +19,10 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
         menu = QtWidgets.QMenu(parent)
 
-        self.create_exit()
         self.list_subfolders(mc_dir, menu)
         self.setContextMenu(menu)
+        menu.addSeparator()
+        self.create_exit(menu)
 
     def create_exit(self, menu):
         '''
@@ -37,7 +37,6 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         '''
         exit()
 
-
     def list_subfolders(self, path, menu):
         '''
             Detects subfolders, calls file lister for main folder.
@@ -50,40 +49,39 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
                 self.list_subfolders(object_path, subfolder_menu)
         self.list_files(path, menu)
 
-    def create_submenu(self, i, menu):
-        '''
-        Create submenu in the menu for subfolder.
-        '''
-        # print ">> Folder : ", object_path
-        subfolder_menu = menu.addMenu(i)
-        return subfolder_menu
-
     def list_files(self, path, menu):
         '''
         Lists files in the folder.
         '''
-         for i in os.listdir(path):
-             object_path = path +'/'+ i
-             if os.path.isfile(object_path) is True:
-                #  print object_path
-                 self.create_file_in_menu(i, menu)
-        #  print '\n'
+        for i in os.listdir(path):
+            object_path = path +'/'+ i
+            if os.path.isfile(object_path) is True:
+                self.create_file_in_menu(object_path, menu) # changed "i" to object_path
+                # changed due to an issue with opening the file, when using "i" - unable to open the file to copy
+                # when clicked, will need to find new solution as it is not feasible to have full filepath for each item
+                # even though with "i" it is being detected that "namehere.txt" is being clicked, unable to find/open file
     
+    def create_submenu(self, i, menu):
+        '''
+        Create submenu in the menu for subfolder.
+        '''
+        subfolder_menu = menu.addMenu(i)
+        return subfolder_menu
+
     def create_file_in_menu(self, i, menu):
         '''
         Create file in the menu.
         '''
         file_menu = menu.addAction(i)
-        file_menu.triggered.connect(self.testing_func)
+        file_menu.triggered.connect(self.copy_contents)
 
-
-    def testing_func(self):
+    def copy_contents(self):
         '''
-        Till the actual functionality is added.
+        Copies the selected files contents to clipboard.
         '''
         snd = self.sender().text()
-        print(f"detected: {snd}")
-
+        txt_to_copy = open((snd), "r").read()
+        pyperclip.copy(txt_to_copy)
 
 def main(image):
     app = QtWidgets.QApplication(sys.argv)
@@ -95,5 +93,5 @@ def main(image):
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    on = r'D:\Python\mc.ico'
-    main(on)
+    on = systray_icon
+main(on)
